5b39b677dbcb3d91f750a7dafedaa2923781276e
Ties Westerborg <t.d.westerborg@student.tudelft.nl>
2015-09-02 17:05:29 +0200

    no message

--- src/application/core/Player.java
+++ src/application/core/Player.java
@@ -1,6 +1,7 @@
 package application.core;
 
 import application.Main;
+import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import org.w3c.dom.Element;
 
@@ -14,12 +15,34 @@ public class Player implements Sprite {
     protected int tY;
     protected Image tImage;
     protected int tHealth;
+    protected AnimationTimer tTimer;
+
+    boolean tShoot, tGoLeft, tGoRight;
 
     public Player() {
         tImage = new Image(new File("src/application/images/smallAlien.png").toURI().toString());
         tX = (int)(Main.getWidth()/2 - tImage.getWidth()/2);
         tY = (int)(Main.getHeight()-tImage.getHeight()-50);
         tHealth = 1;
+        tTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if(tGoLeft) {
+                    moveLeft();
+                }
+                else if(tGoRight) {
+                    moveRight();
+                }
+                else if(tShoot) {
+                    //shoot
+                }
+                try {
+                    Thread.sleep(15);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     public void readXml(Element eElement) {
@@ -38,19 +61,27 @@ public class Player implements Sprite {
         return tImage;
     }
 
-    public void moveLeft() {
+    protected void moveLeft() {
         if ( !((tX - tImage.getWidth() - 10) < 0 )) {
             tX--;
         }
     }
-    public void moveRigth() {
+    protected void moveRight() {
         if ( !((tX + tImage.getWidth() + 10) > Main.getWidth() )) {
             tX++;
         }
     }
 
     public String toString() {
-        String result = "Player on coords: "+ tX +", "+ tY;
+        String result = "Player on coords: " + tX + ", " + tY;
         return result;
+    }
+
+    public void leftArrowPressed(boolean pressed) {
+        tGoLeft = pressed;
+    }
+
+    public void rightArrowPressed(boolean pressed) {
+        tGoRight = pressed;
     }
 }
