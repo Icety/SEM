165c578589048f172d76c5527bc993e322cf7f2b
Arthur Breurkes <a.r.breurkes@student.tudelft.nl>
2015-09-16 17:02:21 +0200

    100% coverage on BossProjectile.java.

--- src/test/java/application/core/BossProjectileTest.java
+++ src/test/java/application/core/BossProjectileTest.java
@@ -1,5 +1,6 @@
 package application.core;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,11 @@ import static org.junit.Assert.*;
 public class BossProjectileTest {
     private BossProjectile testProjectile;
 
+    @Before
+    public void setUp() {
+        testProjectile = new BossProjectile(1, 1);
+    }
+
     /**
      * Test whether the object is correctly initialized.
      *
@@ -17,10 +23,13 @@ public class BossProjectileTest {
      */
     @Test
     public void testForProperInitialization() throws Exception {
-        testProjectile = new BossProjectile(1, 1);
-
         assertNotNull(testProjectile);
         assertEquals(1, testProjectile.getX());
         assertEquals(1, testProjectile.getY());
+    }
+
+    @Test
+    public void testGetImage() throws Exception {
+        assertNull(testProjectile.getImage());
     }
 }
