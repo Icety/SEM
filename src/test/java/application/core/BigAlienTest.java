95845034c405f1ca21d4cc4a50c387c6434f1208
Arthur Breurkes <a.r.breurkes@student.tudelft.nl>
2015-09-16 16:59:41 +0200

    100% coverage for BigAlien.java.

--- src/test/java/application/core/BigAlienTest.java
+++ src/test/java/application/core/BigAlienTest.java
@@ -1,9 +1,13 @@
 package application.core;
 
+import application.Main;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test class for BigAlien.java.
@@ -21,6 +25,21 @@ public class BigAlienTest {
         testAlien = new BigAlien();
     }
 
+    /**
+     * Test whether getImage() returns the correct Image.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetImage() throws Exception {
+        assertNull(testAlien.getImage());
+    }
+
+    /**
+     * Test whether addShootChance() updates the value correctly.
+     *
+     * @throws Exception
+     */
     @Test
     public void testAddShootChance() throws Exception {
         testAlien.addShootChance();
