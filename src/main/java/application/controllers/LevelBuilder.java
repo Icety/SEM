7ec6184b88e7ca107691ba9749fee84ef6cb36e4
Niek van der Laan <niekvanderlaan@outlook.com>
2015-09-17 20:51:41 +0200

    Arrows LevelBuilder
    Due to a problem with the mouselistener in Slick2d, I was unable to implement the LevelBuilder as intended. Instead, I have implemented a level builder which fully runs on keyboard.

--- src/main/java/application/controllers/LevelBuilder.java
+++ src/main/java/application/controllers/LevelBuilder.java
@@ -8,7 +8,9 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.*;
+import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
@@ -23,10 +25,14 @@ import java.util.ArrayList;
 public class LevelBuilder extends BasicGameState {
 
     protected int tId;
-    protected String tBackgroundString;
+    protected Image tBackground;
+    protected String tBackgroundString = "moving.jpg";
     protected Alien selected = new Alien();
-    protected int mousex;
-    protected int mousey;
+    protected int menuHeight;
+    protected Circle circle;
+    protected int circlex;
+    protected int circley;
+    protected boolean circleDown, circleUp, circleLeft, circleRight;
 
     ArrayList<Alien> aliens = new ArrayList<Alien>();
 
@@ -38,32 +44,12 @@ public class LevelBuilder extends BasicGameState {
     public void init(GameContainer container, StateBasedGame game)
             throws SlickException {
 
-        mousex = 0;
-        mousey = 0;
+        circlex = Main.WIDTH/2;
+        circley =  Main.HEIGHT/3;
+        circle = new Circle(circlex,circley, 80);
+        menuHeight = 150;
         selected = null;
-        //Add all aliens to list
-        aliens.add(new MiniAlien());
-        aliens.add(new SmallAlien());
-        aliens.add(new BigAlien());
-        aliens.add(new MothershipAlien());
-        aliens.add(new FinalBoss());
-
-
-        int x;
-        int y = 10;
-        int count = 0;
-        for (Alien alien : aliens) {
-            alien.settY(y);
-            if (count % 2 == 0) {
-                alien.settX(10);
-                alien.getImage().draw(alien.getX(), alien.getY(), alien.getWidth(), alien.getHeight());
-            } else {
-                alien.settX(80);
-                alien.getImage().draw(alien.getX(), alien.getY(), alien.getWidth(), alien.getHeight());
-                y += 70;
-            }
-            count++;
-        }
+        tBackground = new Image("src/main/java/application/images/"+ tBackgroundString);
     }
 
     public int getID() {
@@ -75,47 +61,109 @@ public class LevelBuilder extends BasicGameState {
             throws SlickException {
 
 
+        tBackground.draw(0, 0, container.getWidth(), container.getHeight());
+
+        g.setColor(Color.blue);
+        g.fillRect(0, 0, Main.WIDTH, menuHeight);
+
         g.setColor(Color.white);
 
-        for (Alien alien : aliens) {
-            alien.getImage().draw(alien.getX(), alien.getY(), 70, 70);
+        g.drawString("Selected Alien: ", Main.WIDTH / 4, menuHeight/2);
+
+        if (selected != null) {
+            selected.getImage().draw(Main.WIDTH / 2, menuHeight / 3, menuHeight / 2, menuHeight / 2);
         }
+
+        for (Alien alien : aliens) {
+            alien.getImage().draw(alien.getX(), alien.getY(), alien.getWidth(), alien.getHeight());
+        }
+
+        g.setColor(Color.red);
+        g.draw(circle);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta)
             throws SlickException {
-        mousex = container.getInput().getMouseX();
-        mousey = container.getInput().getMouseY();
-    }
-
-    public void mousePressed(int button, int x, int y) {
-
-    }
-
-    public void mouseReleased(int button, int x, int y) {
-
-        //selecting type of alien
-        if (mousex < 200) {
-            if (button == Input.MOUSE_LEFT_BUTTON) {
-               chooseAlien(checkCollision(mousex, mousey));
-                System.out.println("checked collision");
-            }
-        } else {
-            if (selected != null) {
-                //Place the currently selected alien on the field
-                if (button == Input.MOUSE_LEFT_BUTTON) {
-                    placeAlien(makeAlien(), mousex, mousey);
-                }
-            }
+        if (circleUp && circle.getMinY() > menuHeight) {
+            circley -= 4;
         }
-        if (button == Input.MOUSE_RIGHT_BUTTON) {
-            if (Mouse.getX() > 200) {
-                Alien a = checkCollision(x, y);
-                if (a != null) {
-                    aliens.remove(checkCollision(x, y));
+        if (circleDown && circle.getMaxY() < Main.HEIGHT) {
+            circley += 4;
+        }
+        if (circleRight && circle.getMaxX() < Main.WIDTH) {
+            circlex += 4;
+        }
+        if (circleLeft && circle.getMinX() > 0){
+            circlex -= 4;
+        }
+        circle.setCenterX(circlex);
+        circle.setCenterY(circley);
+    }
+
+    public void keyReleased(int key, char c) {
+        switch(key) {
+            case Input.KEY_1:
+                selected = new MiniAlien();
+                break;
+            case Input.KEY_2:
+                selected = new SmallAlien();
+                break;
+            case Input.KEY_3:
+                selected = new BigAlien();
+                break;
+            case Input.KEY_4:
+               selected = new FinalBoss();
+                break;
+            case Input.KEY_5:
+                selected = new MothershipAlien();
+            default:
+                break;
+            case Input.KEY_DOWN:
+                    circleDown = false;
+                break;
+            case Input.KEY_UP:
+                    circleUp = false;
+                break;
+            case Input.KEY_LEFT:
+                    circleLeft = false;
+                break;
+            case Input.KEY_RIGHT:
+                    circleRight = false;
+                break;
+            case Input.KEY_SPACE:
+                if (selected != null) {
+                    Alien newAlien = makeAlien();
+                    placeAlien(newAlien, circlex - newAlien.getWidth()/2, circley - newAlien.getHeight()/2);
                 }
-            }
+                break;
+            case Input.KEY_R:
+                Alien toRemove = checkCollision();
+                if (checkCollision() != null) {
+                    aliens.remove(toRemove);
+                }
+                break;
+        }
+    }
+
+    public void keyPressed(int key, char c) {
+        switch(key) {
+            case Input.KEY_DOWN:
+                if (circle.getY() < Main.HEIGHT)
+                circleDown = true;
+                break;
+            case Input.KEY_UP:
+                if (circle.getY() > 0)
+                circleUp = true;
+                break;
+            case Input.KEY_LEFT:
+                if (circle.getX() > 0)
+                circleLeft = true;
+                break;
+            case Input.KEY_RIGHT:
+                if (circle.getX() < Main.WIDTH)
+                circleRight = true;
+                break;
         }
     }
 
@@ -128,12 +176,9 @@ public class LevelBuilder extends BasicGameState {
         }
     }
 
-    public Alien checkCollision(int x, int y) {
-        Rectangle mouseBox = new Rectangle(x, y, 10, 10);
-        System.out.println("MOUSE LOCATION: " + x + ", " + y);
+    public Alien checkCollision() {
         for(Alien a: aliens) {
-            System.out.println("ALIEN LOCATION: " + a.getX() + ", " + a.getY());
-            if(a.getBoundingBox().intersects(mouseBox)) {
+            if(a.getBoundingBox().intersects(circle)) {
                 return a;
             }
         }
@@ -142,9 +187,11 @@ public class LevelBuilder extends BasicGameState {
     }
 
     public void placeAlien(Alien alien, int x, int y) {
-        alien.settX(x);
-        alien.settY(y);
-        aliens.add(alien);
+        if (alien != null) {
+            alien.settX(x);
+            alien.settY(y);
+            aliens.add(alien);
+        }
     }
 
     public Alien makeAlien() {
